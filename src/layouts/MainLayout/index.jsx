eb8dae74006ab14d5a592975bd4376944ca12637
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-02-20 23:21:49 -0800

    useDimensions hook for easy responsiveness

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -1,8 +1,12 @@
 import { MobileNavbar } from "components/Navbar";
 
+import { useWindowDimensions } from "hooks/ui_hooks";
+
 import styles from "./MainLayout.module.css";
 
 export const MainLayout = ({ children }) => {
+  const { width, height } = useWindowDimensions();
+  console.log(width)
   return (
     <>
       <MobileNavbar />
