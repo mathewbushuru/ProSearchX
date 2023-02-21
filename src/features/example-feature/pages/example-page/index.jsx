74b095c00d5f27865927be90651025a64472e90d
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-02-21 01:05:39 -0800

    Update feature directories

---
+++ src/features/example-feature/pages/example-page/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ExamplePage = () => {
+  return (
+    <div>index</div>
+  )
+}
---
+++ src/features/search/pages/search-query/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SearchQueryPage = () => {
+  return (
+    <div>index</div>
+  )
+}
