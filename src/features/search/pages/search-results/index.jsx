1c3171a5bbbfd396a390743e1e5aaa17bba8005e
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-02-24 00:43:33 -0800

    add search result details and style for mobile

--- src/features/search/pages/search-results/index.jsx
+++ src/features/search/pages/search-results/index.jsx
@@ -1,21 +1,27 @@
-import React from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+import { BiWorld as LinkIcon } from "react-icons/bi";
+
+import { searchApi } from "features/search/api/search-api";
+import { SEARCH_ON } from "config";
 
 import { SearchResultsLayout } from "features/search/layouts";
 
-import { searchApi } from "features/search/api/search-api";
+import styles from "./SearchResultsPage.module.css";
 
 export const SearchResultsPage = () => {
   const [searchParams] = useSearchParams();
 
   const searchQuery = searchParams.get("q");
 
-  if (searchQuery.length === 0) {
+  if (searchQuery.trim().length === 0) {
     return (
       <SearchResultsLayout>
-        Sorry, could not complete your request because of an empty search
-        string. <br />
-        <Link to="/">Back to query page</Link>
+        <div className={`code ${styles.emptyErrorMessageContainer}`}>
+          <p>
+            Sorry, could not complete your request because of an empty search
+            string.
+          </p>
+        </div>
       </SearchResultsLayout>
     );
   }
@@ -24,33 +30,46 @@ export const SearchResultsPage = () => {
 
   return (
     <SearchResultsLayout>
-      <p>
-        <i style={{ color: "red" }}>
-          ( To protect my scraping rate limit, I have turned search off and i'm
-          using a cached previous query('ubc'). If you are looking at
-          the code, you can turn it back on at /src/config/index.js by changing
-          SEARCH_ON from false to true)
-        </i>
-      </p>
+      <div className={styles.searchResultsContainer}>
+        {searchResults.map((page, pageNumber) => (
+          <div key={pageNumber}>
+            {!SEARCH_ON && (
+              <div
+                className={`code ${styles.searchResult} ${styles.codeDisclaimer}`}
+              >
+                <p>
+                  [To protect my scraping rate limit, I have turned search off
+                  and I'm using a cached previous query('ubc'). If you are
+                  looking at the code, you can turn it back on at
+                  /src/config/index.js by changing SEARCH_ON from false to true]
+                </p>
+              </div>
+            )}
+            {/* <h1 className="code">Page {pageNumber + 1}</h1> */}
+            {/* <p>About {page.estimatedResultCount} results</p> */}
+            {page.items.map((result, index) => (
+              <div key={index} className={styles.searchResult}>
+                <div className={styles.searchResultLinks}>
+                  <LinkIcon className={styles.linkIcon} />
+                  <div>
+                    <div className={styles.displayLink}>
+                      <a href={result.link}>{result.displayLink}</a>
+                    </div>
+                    <div className={styles.link}>
+                      <a href={result.link}>{result.link}</a>
+                    </div>
+                  </div>
+                </div>
 
-      {searchResults.map((page, pageNumber) => (
-        <div key={pageNumber}>
-          <h1>Page {pageNumber + 1}</h1>
-          <p>
-            <strong>Search results:</strong> About {page.estimatedResultCount}{" "}
-            results
-          </p>
-          {page.items.map((result, index) => (
-            <div key={index}>
-              <h3>{result.title}</h3>
-              <a href={result.link}>{result.displayLink}</a>
-              <p>{result.snippet}</p>
-              <hr />
-            </div>
-          ))}
-        </div>
-      ))}
-      <Link to="/">Back to query page</Link>
+                <div className={styles.searchResultDetails}>
+                  <h3>{result.title}</h3>
+                  <p>{result.snippet}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </SearchResultsLayout>
   );
 };
