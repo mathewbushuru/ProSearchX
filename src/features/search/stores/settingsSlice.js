6fb9d09c123209c588ee04475337dec258b361de
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-03-18 15:18:11 -0700

    open in new tab now default

--- src/features/search/stores/settingsSlice.js
+++ src/features/search/stores/settingsSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const settingsSlice = createSlice({
   name: "settings",
   initialState: {
-    newTab: false,
+    newTab: true,
   },
   reducers: {
     toggleNewTabAction: (state) => {
